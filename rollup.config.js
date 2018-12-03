9aa30ee95b1232852c229cb1f3e61bdee6cd569a
Luciano Fantone <luciano@flybondi.com>
2018-12-03 17:40:17 -0300

    chore: prepare package to upload to npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import buble from 'rollup-plugin-buble'
 
-var external = Object.keys( require( './package.json' ).dependencies ).concat( 'path' )
+const external = Object.keys( require( './package.json' ).dependencies ).concat( 'path' )
 
 export default {
     entry: 'src/index.js',
